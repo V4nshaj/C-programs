d96203ca73a00a97cd632d7a9f710797546fec88
V4nshaj <95959007+V4nshaj@users.noreply.github.com>
2023-07-04 23:57:30 +0530

    Update print.cpp

--- lessons/print.cpp
+++ lessons/print.cpp
@@ -5,9 +5,9 @@ like in word we hav doc for word and ppt for powerpoint similarly .h for header 
 /*printf()->its fullform print to file.()paranthesis tells the compiler that this is a func
 "" the part within double coutes will be printed. "" as it prints d string(grp of char)
 puts()->full form put string same as printf */
-/*int main()
+int main()
 {
     printf("Your are awesome\n");//; -> rep end of d statement similar as .(full stop)
     puts("I am the best");
     return 0;
-}*/
+}
